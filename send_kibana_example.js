3a67cf94d3a3a72604d1eba4a00f6d75c341fbe2
haeun1107 <haeun9394@gmail.com>
2025-01-26 20:32:32 +0900

    [Fix] 서울 기준 시간 설정

--- send_kibana_example.js
+++ send_kibana_example.js
@@ -1,12 +1,17 @@
 const axios = require('axios');
+const moment = require('moment-timezone'); // 시간 변환을 위한 모듈
 
 // Kibana(Elasticsearch) 연결 정보
 const ELASTICSEARCH_URL = 'http://34.64.200.202:9200'; // Elasticsearch 기본 URL
 const INDEX_NAME = 'sensor'; // 인덱스 이름
 const kibanaApiUrl = `${ELASTICSEARCH_URL}/${INDEX_NAME}/_doc`; // 엔드포인트 URL
 
-// 전송할 데이터 예시
+// 현재 대한민국 서울 시간을 가져오기
+const seoulTime = moment.tz('Asia/Seoul').format('YYYY-MM-DDTHH:mm:ss.SSSSSS'); // 나노초 포함
+
+// Kibana에 전송할 데이터 예시
 const jsonData = {
+  id: 1, // 데이터 ID
   applicationID: 1001,
   applicationName: "Water Monitoring",
   devEUI: 1241054800,
@@ -18,11 +23,11 @@ const jsonData = {
   NH4: 0,
   salt: 26.83,
   ALCOHOL: 0,
-  createdAt: new Date().toISOString(),
-  updatedAt: new Date().toISOString(),
+  createdAt: seoulTime, // 현재 서울 시간
+  updatedAt: seoulTime, // 현재 서울 시간
 };
 
-// Kibana(Elasticsearch)에 데이터 전송
+// Kibana(Elasticsearch)에 데이터 전송 함수
 async function sendDataToKibana(data) {
   try {
     const response = await axios.post(kibanaApiUrl, data, {
